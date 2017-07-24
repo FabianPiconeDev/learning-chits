1c66e8bd759dbdada40bcd92b2a343bfd2117bdb
Fabian Picone <fabian.picone@gmail.com>
2017-07-24 11:04:07 +0200

    Prepared insert chit command and handler

---
+++ src/Application/CommandHandler/InsertChitHandler.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace FabianPiconeDev\Application\CommandHandler;
+
+final class InsertChitHandler
+{
+
+}
---
+++ src/Domain/Command/InsertChitCommand.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace FabianPiconeDev\Domain\Command;
+
+final class InsertChitCommand
+{
+
+}
